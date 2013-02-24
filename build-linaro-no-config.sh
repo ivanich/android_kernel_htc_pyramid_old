051a016f9931404a7cccdcfc57e5dd40f2db89ed
Matt Filetto <matt.filetto@gmail.com>
2013-02-24 14:44:00 -0800

    updated build script to export included linaro toolchain for compiling
    Change-Id: I51476239f0b6a2e8faf9829bccd0293393445490

--- build-linaro-no-config.sh
+++ build-linaro-no-config.sh
@@ -9,12 +9,16 @@ NOWT=$(date +"%T")
 # Number of jobs (usually the number of cores your CPU has (if Hyperthreading count each core as 2))
 MAKE="4"
 
+# choose defconfig
+echo Checking out pyramid_defconfig
+make pyramid_defconfig
+
 ## Set compiler location
 echo Setting compiler location...
 export ARCH=arm
-export CROSS_COMPILE=$HOME/android/system/prebuilt/linux-x86/toolchain/linaro/bin/arm-cortex_a8-linux-gnueabi-
+export CROSS_COMPILE=toolchains/linaro/bin/arm-cortex_a8-linux-gnueabi-
 
-## Build Sultan kernel
+## Build kernel
 make -j$MAKE ARCH=arm
 sleep 1
 
@@ -26,18 +30,20 @@ sleep 1
      mkdir -p $HOME/KERNEL/
      mkdir -p $HOME/KERNEL/out/
      mkdir -p $HOME/KERNEL/out/system/lib/modules/
+     mkdir -p $HOME/KERNEL/out/system/etc/init.d/
      mkdir -p $HOME/KERNEL/out/kernel/
      mkdir -p $HOME/KERNEL/out/META-INF/
 
 cp -a $(find . -name *.ko -print |grep -v initramfs) $HOME/KERNEL/out/system/lib/modules/
 cp -rf prebuilt-scripts/META-INF/ $HOME/KERNEL/out/
 cp -rf prebuilt-scripts/kernel_dir/* $HOME/KERNEL/out/kernel/
+cp -rc prebuilt-scripts/99kernel $HOME/kernel/out/system/etc/init.d/
 cp arch/arm/boot/zImage $HOME/KERNEL/out/kernel/
 
 # build flashable zip
 
 cd $HOME/KERNEL/out/
-zip -9 -r $HOME/ChronicBruce-dev-$NOW-$NOWT.zip .
+zip -9 -r $HOME/ChronicBruce-$NOW-$NOWT.zip .
 echo Deleting Temp files and folders....
 rm -rf $HOME/KERNEL/
 echo Build Complete, Check your Home directory for a flashable zip
